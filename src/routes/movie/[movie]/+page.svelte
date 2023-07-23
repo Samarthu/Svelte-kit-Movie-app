97dc4152d3b03b94d5cb682b716ac415bb8c1559
Samarth Upare <samarth.upare@elastic.run>
2023-07-23 12:42:57 +0530

    back button added

--- src/routes/movie/[movie]/+page.svelte
+++ src/routes/movie/[movie]/+page.svelte
@@ -1,6 +1,6 @@
 <script>
+    import { goto } from '$app/navigation';
     import { onMount } from 'svelte';
-
 
 
   /** @type {import('./$types').PageData} */
@@ -15,7 +15,7 @@
 </script>
 
 <!-- Showing Movie Detail Page -->
-
+<button type="button" style="float:right" class=" mr-4 m-2 right-0 px-p15 text-sm text-white font-medium py-p10 rounded bg-blue-500 ml-3 ml-auto  bg-bg-blue-500 font-medium rounded text-yellow-50 text-sm px-5 py-2" on:click|preventDefault={()=> goto(window.origin + '/movies')}>Back</button>
 <div class="movie-details">
   <div class="img-container">
     <a href={'https://image.tmdb.org/t/p/original' + data.backdrop_path}>
